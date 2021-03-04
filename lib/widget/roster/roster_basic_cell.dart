67c3b8afb9807ec9d52f2714881a2f5def1abe57
Mario Monzón <hola@mariomonzon.dev>
2021-03-04 20:24:13 +0000

    Roster branch created

---
+++ lib/widget/roster/roster_basic_cell.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RosterBasicCell extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(),
+          Row(),
+        ],
+      ),
+    );
+  }
+}
