04cf81dfe7c55e9959f69d6d4390d02ef947c5c5
Mario Monzón <hola@mariomonzon.dev>
2021-03-06 14:07:32 +0000

    Added Roster list item detail

--- lib/widget/roster/roster_basic_cell.dart
+++ lib/widget/roster/roster_basic_cell.dart
@@ -1,6 +1,16 @@
+import 'package:canes_football_app/design/canes_text_style.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class RosterBasicCell extends StatelessWidget {
+  String playerName;
+  String playerPosition;
+
+  RosterBasicCell({
+    @required this.playerName,
+    this.playerPosition,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,8 +19,27 @@ class RosterBasicCell extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(),
-          Row(),
+          Row(
+            children: [
+              Text(
+                'Nombre: ',
+                style: CanesTextStyle.boldL,
+              ),
+              Text(playerName),
+            ],
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            children: [
+              Text(
+                'Poisición: ',
+                style: CanesTextStyle.boldL,
+              ),
+              Text(playerPosition),
+            ],
+          ),
         ],
       ),
     );
