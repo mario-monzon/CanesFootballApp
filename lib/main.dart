6f1ea09bf012ee822c923939eed57736b681e2db
Mario Monzón <hola@mariomonzon.dev>
2021-03-06 14:28:48 +0000

    Merged all branchs into develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:canes_football_app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    // systemNavigationBarColor: Colors.black, // navigation bar color
+    statusBarColor: Colors.transparent, // status bar color
+  ));
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
